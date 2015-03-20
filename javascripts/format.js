82ee6b6c61843361b4e24a5ad55b0d2d85609501
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-03-20 15:38:13 +0900

    Improve

--- javascripts/format.js
+++ javascripts/format.js
@@ -4,191 +4,183 @@ var QueryFormer = "";
 var QueryLatter = "";
 var CaretPosition = 0;
 var DefaltPrefix = ["rdf:", "rdfs:", "owl:", "dc:", "dcterm:", "skos:", "foaf:", "xsd:"]
-var SparqlTerm = ["FILTER ()", "REGEX ()", "REPLACE ()", "CONTAINS ()", "BIND ()", "BOUND ()", "STR ()", "IF ()", "COALESCE ()", "EXIST {}", "NOT EXIST {}", "SameTerm ()", "IN ()", "NOT IN ()", "IRI ()", "IsIRI ()", "IsBlank ()", "IsLiteral ()", "IsNumeric ()", "LANG ()", "DATATYPE ()", "BNODE ()", "STRDT ()", "STRLANG ()", "UUID ()", "STRUUID ()", "STRLEN ()", "SUBSTR ()", "UCASE ()", "LCASE ()", "STRSTARTS ()", "STRENDS ()", "STRBEFORE ()", "STRAFTER ()", "ENCODE_FOR_URI ()", "CONCAT ()", "LangMatches ()", "ABS ()", "ROUND ()", "CEIL ()", "FLOOR ()", "RAND ()", "NOW ()", "YEAR ()", "MONTH ()", "DAY ()", "HOURS ()", "MINUITES ()", "SECONDS ()", "TIMEZONE ()", "TZ ()", "MD5 ()", "SHA1 ()", "SHA256 ()", "SHA384 ()", "SHA512 ()","PREFIX", "SELECT", "DISTINCT", "FROM", "GRAPH", "WHERE", "UNION", "VALUES", "ORDER BY", "GROUP BY", "LIMIT", "OFFSET", "DESCRIBE", "CONSTRUCT {}", "ASK {}", "MINUS {}", "OPTIONAL {}"];
+var SparqlTerm = ["FILTER ()", "REGEX ()", "REPLACE ()", "CONTAINS ()", "BIND ()", "BOUND ()", "STR ()", "IF ()", "COALESCE ()", "EXIST {}", "NOT EXIST {}", "SameTerm ()", "IN ()", "NOT IN ()", "IRI ()", "IsIRI ()", "IsBlank ()", "IsLiteral ()", "IsNumeric ()", "LANG ()", "DATATYPE ()", "BNODE ()", "STRDT ()", "STRLANG ()", "UUID ()", "STRUUID ()", "STRLEN ()", "SUBSTR ()", "UCASE ()", "LCASE ()", "STRSTARTS ()", "STRENDS ()", "STRBEFORE ()", "STRAFTER ()", "ENCODE_FOR_URI ()", "CONCAT ()", "LangMatches ()", "ABS ()", "ROUND ()", "CEIL ()", "FLOOR ()", "RAND ()", "NOW ()", "YEAR ()", "MONTH ()", "DAY ()", "HOURS ()", "MINUITES ()", "SECONDS ()", "TIMEZONE ()", "TZ ()", "MD5 ()", "SHA1 ()", "SHA256 ()", "SHA384 ()", "SHA512 ()", "PREFIX", "SELECT", "DISTINCT", "REDUCED", "FROM", "GRAPH", "WHERE", "UNION", "VALUES", "ORDER BY", "GROUP BY", "LIMIT", "OFFSET", "DESCRIBE", "CONSTRUCT {}", "ASK {}", "MINUS {}", "OPTIONAL {}"];
 
-var TextAreaID = 'query';
+var TextAreaName = 'query';
 var IndentUnit = 4;
 
+// event
 document.onkeydown = function (e){
-    var current = document.activeElement;
-    var textarea = document.getElementById(TextAreaID);
-    if(current == textarea && e.keyCode == 9) return false;
-    if(current != textarea && e.keyCode == 9) textarea.spellcheck = false;
-    if(current == textarea && TermFrag && e.keyCode == 13){
+    var textarea = document.activeElement;
+    var name = textarea.name;
+    if(name = TextAreaName && e.keyCode == 9) return false;
+    if(name != TextAreaName && e.keyCode == 9) textarea.spellcheck = false;
+    if(name = TextAreaName && TermFrag && e.keyCode == 13){
 	TermFrag = "";
 	return false;
     }
 }
 
+document.onkeyup = function (e){
+    var key_code = e.keyCode;
+    var textarea = document.activeElement;
+    var name = textarea.name;
+    if(name = TextAreaName){
+        // tabulator
+	if(key_code != 9) TermFrag = "";
+	if(key_code == 9) tabKeyUp(textarea);
+    }
+}
+
 document.onclick = function (){
-    var current = document.activeElement;
-    var textarea = document.getElementById(TextAreaID);
-    if(current == textarea){
+    var textarea = document.activeElement;
+    var name = textarea.name;
+    if(name = TextAreaName){
 	TermFrag = "";
 	textarea.spellcheck = false;
     }
 }
 
-document.onkeyup = function (e){
-     var key_code = e.keyCode;
-     var current = document.activeElement;
-     var textarea = document.getElementById(TextAreaID);
-     if(current == textarea){
-         // return
-	 // if(key_code == 13) setIndent(textarea);
-         // tabulator
-	 if(key_code != 9) TermFrag = "";
-	 if(key_code == 9) tabKeyUp(textarea);
-     }
-}
-
-function setIndent(textarea){
-   var position = textarea.selectionStart;
-   var value = textarea.value;
-   var lines = value.substr(0, position).split("\n");
-   var caret_line = lines.length - 1;
-   lines = value.split(/\n/);
-   var formerText = [];
-   var latterText = [];
-   for(i = 0; i < caret_line; i++){ formerText.push(lines[i]); }
-   for(i = caret_line + 1; i < lines.length; i++){ latterText.push(lines[i]); }
-   var caretText = lines[caret_line];
-
-   var beforeLine = "";
-   for(i = 1; i < formerText.length; i++){
-      beforeLine = formerText[formerText.length - i];
-       if(beforeLine.match(/[^\s]/)) break;
-   }
-   var word = beforeLine.replace(/^\s*/, "").replace(/\./, "").replace(/;/, "").replace(/\s*$/, "").replace(/\[[^\]]+\]/g, "[]").replace(/, +/g, "").split(/\s+/);
-   var beforeTmp = beforeLine;
-   var bracket = beforeTmp.replace(/\"[^\"]*\"/g, "").replace(/[^\(\)\{\}\[\]]/g, "").split("");
-   var bracketNum = 0;
-   for(i = 0; i < bracket.length; i++){
-       if(bracket[i].match(/[\(\{\[]/)) bracketNum++;
-       if(bracket[i].match(/[\)\}\]]/)) bracketNum--;
-   }
-   var space_count = 0;
-   if(beforeLine) space_count += beforeLine.replace(/^( *).*$/, "$1").length;
-   if(beforeLine.match(/;\s*$/) && word.length == 3) space_count += IndentUnit;
-   if(beforeLine.match(/\.\s*$/) && word.length == 2) space_count -= IndentUnit;
-   if(caretText.match(/^ *[\)\}\]]/)) space_count -= IndentUnit;
-   space_count += IndentUnit * bracketNum;
-   var space = "";
-   for(i = 0; i < space_count; i++){ space += " ";}
-
-   var caretSpace = caretText.replace(/^( *).*/, "$1");
-
-   if(caretSpace != space){
-       textarea.value = formerText.join("\n") + "\n" + space + caretText.replace(/^ +/,"") + "\n" + latterText.join("\n");
-       var curet = position + space.length - caretSpace.length;
-
-       // dummy key event for jQuery.highlightTextarea
-      // var key_e = document.createEvent("KeyboardEvent");
-      // key_e.initKeyEvent("keydown", true, true, null, false, false, false, false, 65, 0);
-     //  document.getElementById("test").dispatchEvent(key_e);
-
-       textarea.setSelectionRange(curet, curet);
-       //  document.getElementById('test3').innerHTML = word.join("#");
-
-       return 0;
-   }else{
-       return 1;
-   }
-}
 
 function tabKeyUp(textarea){
     var flag = setIndent(textarea);
     if(flag){
 	if(!TermFrag) getTermFrag(textarea);
 	if(!TermFrag) return 0;
-	var variables = [];
-	if(TermFrag.match(/^[A-Z]/)){
-	    Array.prototype.push.apply(variables, SparqlTerm);
-	}else{
-	    Array.prototype.push.apply(variables, getWords(textarea));
-	}
-	variables.unshift(TermFrag.replace(/^\?/, "#"));
-	variables = variables.uniq();
-	var setTermFrag = " " + TermFrag.replace(/^\?/, "#").toLowerCase();
-	var setTermFragTmp = " " + TermFragTmp.toLowerCase();
-	if(setTermFragTmp == " "){
-	    TermFragTmp = TermFrag;
-	    setTermFragTmp = setTermFrag;
-	}
-	var flag2 = "";
-	var flag3 = 0;
-	for(i = 0; i < variables.length; i++){
-	    var string = " " + variables[i].toLowerCase();
-	    var regexp = setTermFrag;
-	    if(string.match(regexp)){
-		if(string == setTermFragTmp){
-		    flag3 = 1;
-		}else if(flag3){
-		    TermFragTmp = variables[i];
-		    break;
-		}
-		if(!flag2) flag2 = variables[i];
-	    }else if(flag2 && !(string.match(regexp))){
-		if(flag2){
-		    TermFragTmp = flag2;
-		}else{
-		    TermFragTmp = TermFrag.replace(/^\?/, "#");
-		}
-	//	break;
-	    }
-	}
-
-        if(TermFragTmp){
-	    textarea.value = QueryFormer + TermFragTmp.replace(/^#/, "?") + QueryLatter;
-	    var curet = CaretPosition - TermFrag.length + TermFragTmp.length;
-	    if(TermFragTmp.match(/[\(\{\[][\)\}\]]$/)) curet--;
-	    textarea.setSelectionRange(curet, curet);
-        }
-//	document.getElementById('test').innerHTML = variables.join(": ");
-//	document.getElementById('test3').innerHTML = TermFrag + ", " + TermFragTmp;
-//    document.getElementById('test2').innerHTML = QueryFormer.replace(/</g, "&lt;").replace(/>/g, "&gt;") + "<br>" + QueryLatter.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+	else autoCompletion(textarea);
     }
 }
 
-function getTermFrag(textarea){
-   var position = textarea.selectionStart;
-   var value = textarea.value;
-   QueryFormer = value.substr(0, position).replace(/( *)([\(\{\[]*)[^ \n]*$/, "$1$2");
-   QueryLatter = value.substr(position);
-   CaretPosition = position;
+function setIndent(textarea){
+    var position = textarea.selectionStart;
+    var value = textarea.value;
+    var lines = value.substr(0, position).split("\n");
+    var caretLine = lines.length - 1;
+    var caretLinePosition = lines[lines.length - 1].length;
+    lines = value.split(/\n/);
+    var formerText = [];
+    var latterText = [];
+    for(var i = 0; i < caretLine; i++){ formerText.push(lines[i]); }
+    for(var i = caretLine + 1; i < lines.length; i++){ latterText.push(lines[i]); }
+    var caretText = lines[caretLine];
+    var caretLineIndent = caretText.replace(/^( *).*/, "$1");
 
-   if(!value.substr(position, 1) || value.substr(position, 1).match(/^[\s\)\}\]]/)){
-       TermFrag = value.substr(0, position).replace(/\n/g, " ").replace(/^.+ ([^ ]*)$/, "$1").replace(/^[\(\{\[]+/, "").replace(/[\)\}\]]+$/, "");
-       if(TermFrag.match(/^\^/)) QueryFormer = QueryFormer + "^";
-       TermFrag = TermFrag.replace(/^\^/, "");
-       if(TermFrag.match(/[\.\)\}\]]$/)) TermFrag = "";
-   }else{
-       TermFrag = "";
-   }
+    var beforeLine = "";
+    for(var i = 1; i < formerText.length; i++){
+	beforeLine = formerText[formerText.length - i];
+	if(beforeLine.match(/[^\s]/) && !beforeLine.match(/^\s*#/)) break;
+    }
+
+    var spaceCount = 0;
+    var words = beforeLine.replace(/^\s*/, "").replace(/\./, "").replace(/;/, "").replace(/\s*$/, "").replace(/\[[^\]]+\]/g, "[]").replace(/, +/g, "").split(/\s+/);
+    var beforeTmp = beforeLine;
+    var bracket = beforeTmp.replace(/\"[^\"]*\"/g, "").replace(/[^\(\)\{\}\[\]]/g, "").split("");
+    var bracketNum = 0;
+    for(var i = 0; i < bracket.length; i++){
+	if(bracket[i].match(/[\(\{\[]/)) bracketNum++;
+	if(bracket[i].match(/[\)\}\]]/)) bracketNum--;
+    }
+    if(beforeLine) spaceCount += beforeLine.replace(/^( *).*$/, "$1").length;
+    if(beforeLine.match(/;\s*$/) && words.length == 3) spaceCount += IndentUnit;
+    if(beforeLine.match(/\.\s*$/) && words.length == 2) spaceCount -= IndentUnit;
+    if(caretText.match(/^ *[\)\}\]]/)) spaceCount -= IndentUnit;
+    if(bracketNum < 0) bracketNum++ ;
+    spaceCount += IndentUnit * bracketNum;
+
+    var indent = "";
+    for(var i = 0; i < spaceCount; i++){ indent += " ";}
+    var flag = 1;
+    var caret = position;
+    if(caretLineIndent != indent){
+	textarea.value = formerText.join("\n") + "\n" + indent + caretText.replace(/^ +/,"") + "\n" + latterText.join("\n");
+	caret = position + indent.length - caretLineIndent.length;
+	flag = 0;
+    }else{
+	if(caretText.match(/^\s*$/)) caret = position + indent.length - caretLinePosition;
+    }
+    textarea.setSelectionRange(caret, caret);
+    return flag;
+}
+
+function getTermFrag(textarea){
+    var position = textarea.selectionStart;
+    var value = textarea.value;
+    QueryFormer = value.substr(0, position).replace(/( *)([\(\{\[]*)[^ \n\/\^]*$/, "$1$2");
+    QueryLatter = value.substr(position);
+    CaretPosition = position;
+
+    if(!value.substr(position, 1) || value.substr(position, 1).match(/^[\s\)\}\]]/)){
+	TermFrag = value.substr(0, position).replace(/\n/g, " ").replace(/^.+[ \/\^]([^ ]*)$/, "$1").replace(/^[\(\{\[]+/, "").replace(/[\)\}\]]+$/, "");
+	if(TermFrag.match(/[\.\)\}\]]$/)) TermFrag = "";
+    }else{
+	TermFrag = "";
+    }
     TermFragTmp = "";
- //   document.getElementById('test3').innerHTML = TermFrag + ", " + TermFragTmp + ", " + position;
- //   document.getElementById('test2').innerHTML = QueryFormer + "<br>" + QueryLatter;
+}
+
+function autoCompletion(textarea){
+    var terms = [];
+    if(TermFrag.match(/^[A-Z]/)){
+	Array.prototype.push.apply(terms, SparqlTerm);
+    }else{
+	Array.prototype.push.apply(terms, getWords(textarea));
+    }
+    terms.unshift(TermFrag.replace(/^\?/, "#"));
+    terms = terms.uniq();
+    var setTermFrag = " " + TermFrag.replace(/^\?/, "#").toLowerCase();
+    var setTermFragTmp = " " + TermFragTmp.toLowerCase();
+    if(setTermFragTmp == " "){
+	TermFragTmp = TermFrag;
+	setTermFragTmp = setTermFrag;
+    }
+    var flag2 = "";
+    var flag3 = 0;
+    for(var i = 0; i < terms.length; i++){
+	var string = " " + terms[i].toLowerCase();
+	var regexp = setTermFrag;
+	if(string.match(regexp)){
+	    if(string == setTermFragTmp){
+		flag3 = 1;
+	    }else if(flag3){
+		TermFragTmp = terms[i];
+		break;
+	    }
+	    if(!flag2) flag2 = terms[i];
+	}else if(flag2 && !(string.match(regexp))){
+	    if(flag2){
+		TermFragTmp = flag2;
+	    }else{
+		TermFragTmp = TermFrag.replace(/^\?/, "#");
+	    }
+	}
+    }
+    if(TermFragTmp){
+	textarea.value = QueryFormer + TermFragTmp.replace(/^#/, "?") + QueryLatter;
+	var caret = CaretPosition - TermFrag.length + TermFragTmp.length;
+	if(TermFragTmp.match(/[\(\{\[][\)\}\]]$/)) caret--;
+	textarea.setSelectionRange(caret, caret);
+    }
 }
 
 function getWords(textarea){
-   var query = textarea.value.replace(/\s+/g, " ");
-   var words = query.split(/[ \(\)\{\}\[\],\/]+/);
-   var tmp1 = DefaltPrefix;
-   var tmp2 = [];
-   var tmp3 = [];
-   for(i = 0; i < words.length; i++){
-     words[i] = words[i].replace(/\.$/, "");
-     if(words[i].match(/^\?/)){
-       tmp1.push(words[i].replace(/^\?/, "#"));
-     }else if(words[i].match(/^\w+:$/)){
-       tmp2.push(words[i]);
-     }else if(words[i].match(/^\w+:\w+/)){
-       tmp3.push(words[i]);
-       tmp2.push(words[i].split(/:/)[0] + ":");
-     }
-   }
-   var variables = tmp1.uniq().sort().concat(tmp2.uniq().sort()).concat(tmp3.uniq().sort());
-   return variables;
+    var words = textarea.value.replace(/\s+/g, " ").split(/[ \(\)\{\}\[\],\/]+/);
+    var variable = [];
+    var prefix = [];
+    var prefix2 = [];
+    Array.prototype.push.apply(prefix, DefaltPrefix);
+    for(var i = 0; i < words.length; i++){
+	words[i] = words[i].replace(/\.$/, "");
+	if(words[i].match(/^\?/)){
+	    variable.push(words[i].replace(/^\?/, "#"));
+	}else if(words[i].match(/^\w+:$/)){
+	    prefix.push(words[i]);
+	}else if(words[i].match(/^\w+:\w+/)){
+	    prefix2.push(words[i]);
+	    prefix.push(words[i].split(/:/)[0] + ":");
+	}
+    }
+    var terms = variable.uniq().sort().concat(prefix.uniq().sort()).concat(prefix2.uniq().sort());
+    return terms;
 }
 
 function highlight(){
@@ -205,8 +197,7 @@ function highlight(){
 /*	}, {
 	    color: '#eeeeee',
 	    words: ['# [^\\n]+']    */
-        }],
-        resizable: true
+        }]
     });
 }
 
